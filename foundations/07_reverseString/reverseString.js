ea7dc7039687e0113598f099e036e13a0a4c1075
abbasikhadija <khadijaabbasi.bscsf24@iba-suk.edu.pk>
2026-03-22 16:47:12 +0500

    reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr= str.split("");
+    let reversedArr= arr.reverse();
+    let reversedStr= reversedArr.join("");
+    return reversedStr;
 
 };
 
